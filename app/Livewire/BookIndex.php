796253e7cb1dc6aec7fca7078d4c7012b98646ec
Takeo <urkn99343@me.pikara.ne.jp>
2024-09-02 14:07:26 +0900

    完成

--- app/Livewire/BookIndex.php
+++ app/Livewire/BookIndex.php
@@ -4,17 +4,119 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Book;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Storage;//画像を削除するため
 
 class BookIndex extends Component
 {   
-    public $liveModal = false;
+    use WithFileUploads;//ファイルのアップロードに必要
+    use WithPagination;//別途追加　BookIndexクラスに追加
+    public $liveModal = false;//モーダルウィンドウ
     public $title;
     public $newImage;
     public $price;
     public $description;
+    public $id;
+    public $oldImage;
+    public $editWork = false;//更新するかどうかに必要
+    public $search = '';
+
+
+
+    public function showBookModal(){
+        $this->reset();
+        $this->liveModal = true;
+    }
+
+    
+    public function bookPost(){
+
+        $this->validate([
+
+            'title' => 'required',
+            'newImage' => 'image|max:2048',
+            'price' => 'integer|required',
+            'price' => 'integer|required',
+            'description' => 'required',
+        ]);
+        $image = $this->newImage->store('public/books');
+        Book::create([
+            'title' => $this->title,
+            'image' => $image,
+            'price' => $this->price,
+            'description' => $this->description,
+        ]);
+        $this->reset();
+    }
+
+    public function showEditBookModal($id){
+
+        $book = Book::findOrFail($id);
+        $this-> id = $book->id;
+        $this-> title = $book->title;
+        $this-> oldImage = $book->image;
+        $this-> price = $book->price;
+        $this-> description = $book->description;
+        $this-> editWork  = true;
+        $this-> liveModal = true;
+    }
+
+    public function updateBook($id){
+
+        $this->validate([
+
+            'title' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+        ]);
+
+        if($this->newImage){
+            $image = $this->newImage->store('public/books');
+            Book::where('id', $id)->update([
+
+                'title' => $this->title,
+                'image' => $image,
+                'price' => $this->price,
+                'description' => $this->description,
+            ]);
+
+        }else{
+            Book::where('id', $id)->update([
+
+                'title' => $this->title,
+                'price' => $this->price,
+                'description' => $this->description,
+            ]);
+        }
+        session()->flash('message', '更新しました!');
+    }
+
+    public function deleteBook($id){
+
+        $book = Book::findOrFail($id);
+        Storage::delete($book->image);
+        $book->delete();
+        $this->reset();
+    }
+
 
     public function render()
-    {
-        return view('livewire.book-index');
+    {   
+        if(!empty($this->search)){
+  
+            return view('livewire.book-index',[
+
+                'books' => Book::where('title', 'like', '%' . $this->search . '%')
+                ->orderBy('id', 'DESC')->paginate(3),
+            ]);
+        }else{
+
+            return view('livewire.book-index', [
+                'books' => Book::select('id', 'title', 'price', 'image', 'description')
+                ->orderBy('id', 'DESC')
+                ->paginate(3),
+            ]);
+        }
     }
 }
--- resources/views/livewire/book-index.blade.php
+++ resources/views/livewire/book-index.blade.php
@@ -1,11 +1,94 @@
 <div class="max-w-6xl mx-auto">
     <div class="text-right m-2 p-2">
-        <x-button class="bg-blue-600" wire:click="showBookModal">登録</x-button>
+        <input type="text" wire:model.live="search" id="search" class="border-gray-300 rounded-md" placeholder="キーワード">
+        <x-button class="bg-blue-600 mt-2" wire:click="showBookModal">登録</x-button>
     </div>
 
-    <x-confirmation-modal wire:model="liveModal">
-        <x-slot name="title"><h2 class="text-green-600">登録</h2></x-slot>
-        <x-slot name="content">内容</x-slot>
-        <x-slot name="footer">フッター</x-slot>
+    
+    <div class="m-2 p-2">
+            <table class="w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th class="p-4 text-gray-500 text-left">Id</th>
+                        <th class="p-4 text-gray-500 text-left">Title</th>
+                        <th class="p-4 text-gray-500 text-left">Image</th>
+                        <th class="p-4 text-gray-500 text-left">Price</th>
+                        <th class="p-4 text-gray-500 text-left">Description</th>
+                        <th class="p-4 text-gray-500 text-right">Edit</th>
+                    </tr>
+                </thead>
+                <tbody class="w-full divide-y divide-gray-200">
+                    @foreach($books as $book)
+                        <tr>
+                            <td class="p-4 whitespce-nowrap">{{$book->id}}</td>
+                            <td class="p-4 whitespce-nowrap">{{$book->title}}</td>
+                            <td class="p-4 whitespace-nowrap">
+                                <img class="w-12 h-9 rounded" src="{{Storage::url($book->image)}}" />
+                            </td>
+                            <td class="p-4 whitespace-nowrap">{{ $book->price }}</td>
+                            <td class="p-4 whitespace-nowrap">{!! nl2br($book->description) !!}</td>
+                            <td class="p-4 text-right text-sm">
+                               <x-button class="bg-green-600" wire:click="showEditBookModal({{$book->id}})">編集</x-button> 
+                               <x-button class="bg-red-400" wire:click="deleteBook({{$book->id}})">削除</x-button> 
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="m-2 p-2">{{$books->links()}}</div>
+        </div>
+
+        <x-confirmation-modal wire:model="liveModal">
+
+            @if($editWork)
+                <x-slot name="title"><h2 class="text-green-600">編集</h2></x-slot>
+            @else
+                <x-slot name="title"><h2 class="text-blue-600">登録</h2></x-slot>
+            @endif
+        <x-slot name="content">
+
+            @if(Session()->has('message'))
+                <h3 class="p-2 text-2xl text-green-600">{{session('message')}}</h3>
+            @endif
+
+            <form action="" enctype="multipart/form-data">
+
+                <x-label for="title" value="Title"></x-label>
+                <input type="text" class="block w-full bg-white border border-gyay-400 rounded-md" id="title" wire:model.lazy="title">
+                @error('title') <span class="error text-red-400">{{$message}}</span>@enderror
+
+                <x-label for="image" value="Book Image" class="mt-2"></x-label>
+                @if($newImage)
+                    Photo Preview:
+                    <img src="{{$newImage->temporaryUrl()}}" class="w-48">
+                @else
+                    @if($oldImage)
+                        <img src="{{Storage::url($oldImage)}}" class="w-48">
+                    @endif
+                @endif
+                <input type="file" class="block w-full bg-white border border-gyay-400 rounded-md" id="image" 
+                wire:model.lazy="newImage" accept="image/jpg, image/jpeg, image/png">
+                @error('newImage') <span class="error text-red-400">{{$message}}</span>@enderror
+
+                
+                <x-label for="price" value="Price" class="mt-2"></x-label>
+                <input type="number" class="block w-full bg-white border border-gyay-400 rounded-md"
+                 id="price" wire:model.lazy="price">
+                @error('price') <span class="error text-red-400">{{$message}}</span>@enderror
+                
+                <x-label for="description" value="Description" class="mt-2"></x-label>
+                <textarea name="" class="block w-full bg-white border border-gyay-400 rounded-md"
+                id="description" wire:model.lazy="description"></textarea>
+                @error('description') <span class="error text-red-400">{{$message}}</span>@enderror
+            </form>
+        </x-slot>
+
+        <x-slot name="footer">
+            @if($editWork)
+                <x-button wire:click="updateBook({{$id}})">編集実行</x-button>
+            @else
+                <x-button wire:click="bookPost">登録実行</x-button>
+            @endif
+        </x-slot>
     </x-confirmation-modal>
 </div>
